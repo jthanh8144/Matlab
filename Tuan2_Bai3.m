06a0db1519b3cab7771e81b189044aab014a1142
VoVanThanh <thanhvo618@gmail.com>
2021-09-18 00:02:54 +0700

    1st

---
+++ Tuan2_Bai3.m
@@ -0,0 +1,10 @@
+[x, Fs] = audioread('D:\Down\Kì 5\XLTH\TinHieuMau\lab_female.wav');
+sound_duration = length(x)/Fs;
+t = 0:1/Fs:sound_duration;
+t = t(1:end-1);
+plot(t, x);
+xlabel('Time');
+ylabel('Audio Signal');
+sound(x, Fs);
+sound(x, Fs/2);
+sound(x, Fs*2);
