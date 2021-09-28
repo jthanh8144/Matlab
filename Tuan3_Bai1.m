907f13d9716192b0a5852d238e8d49720e981b39
VoVanThanh <thanhvo618@gmail.com>
2021-09-28 23:46:42 +0700

    mid-term test

--- Tuan3_Bai1.m
+++ Tuan3_Bai1.m
@@ -1,6 +1,6 @@
-n = -4:4;
-x_n = 2*n;
-E = sum(abs(x_n).^2);
+clear;
+xn = audioread('D:\Down\Kì 5\XLTH\TinHieuMau\test\LA001.wav');
+E = sum(abs(xn).^2);
 display(E);
-P = E/length(n);
+P = E / length(xn);
 display(P);
